a5d83a246fc015a0230d8d8762ab1745c39399f9
Vo Minh Thu <thu@hypered.io>
2020-09-14 22:34:12 +0200

    Add default.nix.

---
+++ default.nix
@@ -0,0 +1,6 @@
+{
+  index                   = ./index.md;
+  expose-local-server     = ./expose-local-server.md;
+  starting-with-nixops-1  = ./starting-with-nixops-1.md;
+  starting-with-nixops-2  = ./starting-with-nixops-2.md;
+}
